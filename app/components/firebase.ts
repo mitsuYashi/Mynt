37e99d64c8ffe024af1981904208d95dc3ff6535
HayashiMitsu <hm20011027@gmail.com>
2021-12-03 14:12:40 +0900

    add:環境構築あれこれ

--- app/components/firebase.ts
+++ app/components/firebase.ts
@@ -1,11 +1,7 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import firebase from "firebase/app";
+import "firebase/auth";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyAPiJHG_iYo-DOGasX1D4bJKMx2w7UmdkI",
   authDomain: "hew-project-25587.firebaseapp.com",
@@ -13,9 +9,11 @@ const firebaseConfig = {
   storageBucket: "hew-project-25587.appspot.com",
   messagingSenderId: "821584206223",
   appId: "1:821584206223:web:9ca05f950c4846350a53fa",
-  measurementId: "G-CRJCPMV4JK"
+  measurementId: "G-CRJCPMV4JK",
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const app = firebase.initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+
+export default firebase;
