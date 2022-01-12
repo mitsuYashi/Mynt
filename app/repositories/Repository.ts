bcc945d5807627e5dfbecf71aabdc58bb6cf2829
mitsuYashi <hm20011027@gmail.com>
2022-01-12 17:04:33 +0900

    fix:baseURLを修正

--- app/repositories/Repository.ts
+++ app/repositories/Repository.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseDomain: string = "localhost:3000";
+const baseDomain: string = "http://localhost:3000";
 const Repository = axios.create({
   baseURL: baseDomain,
   withCredentials: true,
