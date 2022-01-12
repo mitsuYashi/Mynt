673bf9b77dae6fab0f86979e3b1e3caa1a2aa534
mitsuYashi <hm20011027@gmail.com>
2022-01-12 17:03:30 +0900

    add:ユーザー登録機能追加

--- app/pages/login.tsx
+++ app/pages/login.tsx
@@ -3,25 +3,35 @@ import type { NextPage } from "next";
 import { useEffect, useState } from "react";
 import Router from "next/router";
 
-import {firebase} from "../components/firebase";
+import { firebase } from "../components/firebase";
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { async } from "@firebase/util";
 
-import {RepositoryFactory} from "../repositories/RepositoryFactory";
-const userRepository = RepositoryFactory.get('users');
+import { RepositoryFactory } from "../repositories/RepositoryFactory";
+const userRepository = RepositoryFactory.get("users");
+const mentaRepository = RepositoryFactory.get("menta");
 
 const Login: NextPage = () => {
   useEffect(() => {
     // console.log(firebase);
   }, []);
 
-  const userPost = async () => {
-    const createResponse = await userRepository.post({user:{
+  const userPost = async (uid: string, mail: string, username: string) => {
+    const createResponse = await userRepository.post({ user: {
+      uuid: uid,
+      mail: mail,
+      name: username,
+    }});
+  };
+  const mentaPost = async (uid: string, mail:string, username: string) => {
+    const createResponse = await mentaRepository.post({menta: {
+      uuid: uid,
+      mail: mail,
+      name: username,
+    }});
+  };
 
-    }})
-  }
-
-  const clickButton = () => {
+  const userButton = () => {
     const provider = new GoogleAuthProvider();
     const auth = getAuth(firebase);
     signInWithPopup(auth, provider)
@@ -31,6 +41,26 @@ const Login: NextPage = () => {
         } else {
           const token = credential.accessToken;
           const user = result.user;
+          userPost(user.uid, user.email ? user.email : '', user.displayName ? user.displayName: 'unknown');
+          Router.push("/home");
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
+
+  const mentaButton = () => {
+    const provider = new GoogleAuthProvider();
+    const auth = getAuth(firebase);
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        if (credential == null) {
+        } else {
+          const token = credential.accessToken;
+          const user = result.user;
+          mentaPost(user.uid, user.email ? user.email : '', user.displayName ? user.displayName: 'unknown');
           Router.push("/home");
         }
       })
@@ -42,7 +72,8 @@ const Login: NextPage = () => {
   return (
     <div>
       <h1>login</h1>
-      <button onClick={clickButton}>Googleでログイン</button>
+      <button onClick={mentaButton}>メンターで登録</button>
+      <button onClick={userButton}>一般ユーザーで登録</button>
     </div>
   );
 };
