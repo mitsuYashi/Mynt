2021ec795356da0aa936f87888afd5d56fd798e9
mitsuYashi <hm20011027@gmail.com>
2022-01-12 11:35:57 +0900

    add:firebaseLogin

--- app/pages/login.tsx
+++ app/pages/login.tsx
@@ -1,43 +1,47 @@
 import axios from "axios";
 import type { NextPage } from "next";
 import { useEffect, useState } from "react";
-import firebase from "../components/firebase";
+import Router from "next/router";
+
+import {firebase} from "../components/firebase";
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { async } from "@firebase/util";
 
-interface State {
-  num: number[];
-}
-
-const initialState: State = {
-  num: [],
-};
+import {RepositoryFactory} from "../repositories/RepositoryFactory";
+const userRepository = RepositoryFactory.get('users');
 
 const Login: NextPage = () => {
   useEffect(() => {
-    console.log(firebase);
+    // console.log(firebase);
   }, []);
+
+  const userPost = async () => {
+    const createResponse = await userRepository.post({user:{
+
+    }})
+  }
 
   const clickButton = () => {
     const provider = new GoogleAuthProvider();
-    const auth = getAuth();
-    try {
-      signInWithPopup(auth, provider)
-        .then((result) => {
-          // console.log("Googleアカウントでログインしました。");
-          console.log(result.user);
-        })
-        .catch((error) => {
-          console.error(error);
-        });
-    } catch (err) {
-      console.log(err);
-    }
+    const auth = getAuth(firebase);
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        if (credential == null) {
+        } else {
+          const token = credential.accessToken;
+          const user = result.user;
+          Router.push("/home");
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+      });
   };
 
   return (
     <div>
-      <h1>test</h1>
+      <h1>login</h1>
       <button onClick={clickButton}>Googleでログイン</button>
     </div>
   );
