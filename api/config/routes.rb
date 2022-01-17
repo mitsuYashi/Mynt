5b1e432dd55a73dcbeb63ada5fec2e6b931030c9
ryuichi <ryuichi19633811@gmail.com>
2022-01-17 17:14:55 +0900

    fix: コンフリクトの治療

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,9 +1,4 @@
 Rails.application.routes.draw do
-  get 'users_tags/index'
-  get 'users_tags/create'
-  get 'users_tags/show'
-  get 'users_tags/destroy'
-  get 'tags/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :example
   resources :users
