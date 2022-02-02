9c4dc0e61388d8eb137f90fe3b7398a5772e4850
ryuichi <ryuichi19633811@gmail.com>
2022-02-02 11:55:54 +0900

    change: 機能の変更

--- api/app/controllers/clients_tags_controller.rb
+++ api/app/controllers/clients_tags_controller.rb
@@ -1,27 +1,33 @@
 class ClientsTagsController < ApplicationController
   def index
-    client_tag = Client_tag.where(params[:user_id])
-    render json: client_tags
+      client_tag = Client_tag.where(params[:client_id])
+      render json: client_tag
   end
 
   def create
-    if client_tag = Client_tag.where(user_id: create_client_tag_param[:user_id], tag_id: create_client_tag_param[:tag_id])
-    else
-        client_tag = Client_tag.create(create_client_tag_param)
-    end
+      if client_tag = Client_tag.where(client_id: create_client_tag_param[:client_id], tag_id: create_client_tag_param[:tag_id])
+      else
+          client_tag = Client_tag.create(create_client_tag_param)
+      end
   end
 
+  def update
+      if client_tag = Client_tag.where(client_id: create_client_tag_param[:client_id], tag_id: create_client_tag_param[:tag_id])
+      end
+  end
+  
   def show
-    client_tags = Client_tag.where(tag_id: params[:tag_id])
-    render json: client_tags
+      client_tags = Client_tag.where(tag_id: params[:tag_id])
+      render json: client_tags
   end
 
   def destroy
-    client_tag.delete(params[:id])
+      client_tag.delete(params[:id])
   end
-
+  
+  
   private
   def create_client_tag_param
-    param.require(:client_tag).permit(:tag_id)
+      param.require(:client_tags).permit(:client_id, :tag_id)
   end
 end
