854d07569d01912f5d0308c5de0473ee36720568
ryuichi <ryuichi19633811@gmail.com>
2022-02-02 10:53:44 +0900

    add: clients_tag 機能の実装

--- api/app/controllers/clients_tags_controller.rb
+++ api/app/controllers/clients_tags_controller.rb
@@ -1,2 +1,27 @@
 class ClientsTagsController < ApplicationController
+  def index
+    client_tag = Client_tag.where(params[:user_id])
+    render json: client_tags
+  end
+
+  def create
+    if client_tag = Client_tag.where(user_id: create_client_tag_param[:user_id], tag_id: create_client_tag_param[:tag_id])
+    else
+        client_tag = Client_tag.create(create_client_tag_param)
+    end
+  end
+
+  def show
+    client_tags = Client_tag.where(tag_id: params[:tag_id])
+    render json: client_tags
+  end
+
+  def destroy
+    client_tag.delete(params[:id])
+  end
+
+  private
+  def create_client_tag_param
+    param.require(:client_tag).permit(:tag_id)
+  end
 end
