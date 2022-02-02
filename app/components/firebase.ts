16c20db4f48db740e82cc3f51b6c7cf0c50d4da5
mitsuYashi <hm20011027@gmail.com>
2022-02-03 01:59:03 +0900

    add:メッセージ機能用関数作成

--- app/components/firebase.ts
+++ app/components/firebase.ts
@@ -1,7 +1,16 @@
 import { initializeApp } from "firebase/app";
-import fb from "firebase/app";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
-import { Router } from "next/router";
+import {
+  addDoc,
+  collection,
+  doc,
+  getDocs,
+  getFirestore,
+  onSnapshot,
+  query,
+  setDoc,
+  where,
+} from "firebase/firestore";
 
 const config = {
   apiKey: "AIzaSyCrPWdNBx4hszLkBOy4dG4Cw6XrdZaO0W8",
@@ -18,16 +27,59 @@ const config = {
 // export const firestore = firebase.firestore();
 
 export const firebase = initializeApp(config);
+export const db = getFirestore();
 
+type message = {
+  created_at: {
+    seconds: number;
+    nanoseconds: number;
+  };
+  uid: string;
+  sendUid: string;
+  message: string;
+};
 
-export const listenAuthState = (firebase: any) => new Promise<string>((resolve, reject) => {
-  const auth = getAuth(firebase);
-  onAuthStateChanged(auth, (user) => {
-    if (user) {
-      
-      const uid = user.uid;
-      resolve(user.uid);
-    }
+export const addMessage = async (
+  uid: string,
+  toUid: string,
+  message: string
+) => {
+  try {
+    const userIds = [uid, toUid].sort();
+    const id = userIds[0] + userIds[1];
+    const docRef = await addDoc(collection(db, id), {
+      user: uid,
+      message: message,
+      created_at: new Date(),
+    });
+    console.log("Document wiriten");
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+// データの取得のみおこなう
+// not use
+export const getMessage = async (uid: string, toUid: string) => {
+  let messages: message[] = [];
+  const userIds = [uid, toUid].sort();
+  const id = userIds[0] + userIds[1];
+  const messagesRef = collection(db, "aaa");
+  const res = await getDocs(messagesRef);
+  res.forEach((doc) => {
+    messages.push(doc.data() as message);
   });
-});
 
+  return messages;
+};
+
+export const listenAuthState = (firebase: any) =>
+  new Promise<string>((resolve, reject) => {
+    const auth = getAuth(firebase);
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        const uid = user.uid;
+        resolve(user.uid);
+      }
+    });
+  });
