a4bf3cc0107f8649a1145cfeb0f57e5236f73cb3
mitsuYashi <hm20011027@gmail.com>
2022-03-03 19:07:30 +0900

    add:model

--- api/app/models/menta_tag.rb
+++ api/app/models/menta_tag.rb
@@ -1,4 +1,4 @@
 class MentaTag < ApplicationRecord
-    # has_many :tag, foreign_key: "tag_id"
-    # has_many :client, foreign_key: "menta_id"
+    has_many :tag, foreign_key: "tag_id"
+    has_many :client, foreign_key: "menta_id"
 end
