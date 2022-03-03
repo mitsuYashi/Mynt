6ae579e0bf29f1f6c1591a6442bd23621b0616a4
mitsuYashi <hm20011027@gmail.com>
2022-03-03 19:07:51 +0900

    add:db

--- api/db/schema.rb
+++ api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_24_113442) do
+ActiveRecord::Schema.define(version: 2022_03_02_014824) do
 
   create_table "client_tags", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "client_id"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 2022_02_24_113442) do
     t.string "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "price", default: 0
   end
 
   create_table "creditcards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
