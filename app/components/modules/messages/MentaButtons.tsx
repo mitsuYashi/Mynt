b03945158f1f77153f430f10799bfad8525fb85d
mitsuYashi <hm20011027@gmail.com>
2022-03-03 19:09:40 +0900

    add:MentaButtons

---
+++ app/components/modules/messages/MentaButtons.tsx
@@ -0,0 +1,283 @@
+import React, { useEffect, useState } from "react";
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  TextField,
+} from "@mui/material";
+import { RepositoryFactory } from "../../../repositories/RepositoryFactory";
+import Router from "next/router";
+
+const noneRepository = RepositoryFactory.get("nones");
+const contractRepository = RepositoryFactory.get("contracts");
+
+type Props = {
+  sendProfile: {
+    user_id: string;
+    name: string;
+    birth: string;
+    profile: string;
+    url: string;
+  };
+  myuid: string;
+};
+
+const MentaButtons: React.FC<Props> = ({ myuid, sendProfile }) => {
+  const [openNone, setOpenNone] = useState(false);
+  const [openContract, setOpenContract] = useState(false);
+  const [price, setPrice] = useState(0);
+  const [contractStatus, setContractStatus] = useState("");
+
+  const nonePost = async () => {
+    try {
+      const res = await noneRepository.post({
+        none: {
+          menta_id: myuid,
+          client_id: sendProfile?.user_id,
+        },
+      });
+      // console.log(res.data);
+      //   Router.push("/home");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const contractPost = async () => {
+    try {
+      const res = await contractRepository.post({
+        contract: {
+          client_id: sendProfile?.user_id,
+          menta_id: myuid,
+          price: price,
+          status: "unpaid",
+        },
+      });
+      setContractStatus("unpaid");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const contractGet = async () => {
+    try {
+      const res = await contractRepository.get({
+        params: {
+          menta_id: myuid,
+          client_id: sendProfile.user_id,
+        },
+      });
+      setContractStatus(res.data.status);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const contractIntervalGet = () => {
+    setInterval(async () => {
+      try {
+        const res = await contractRepository.get({
+          params: {
+            menta_id: myuid,
+            client_id: sendProfile.user_id,
+          },
+        });
+        setContractStatus(res.data.status);
+      } catch (err) {
+        console.log(err);
+      }
+    }, 10000);
+  };
+
+  useEffect(() => {
+    contractGet();
+    contractIntervalGet();
+  }, [myuid]);
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "space-around",
+        margin: "30px auto 0 0",
+      }}
+    >
+      {contractStatus == "success" ? (
+        <>
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={() => {
+              setOpenContract(true);
+            }}
+          >
+            MENTA受諾
+          </Button>
+          <Dialog
+            open={openContract}
+            onClose={() => {
+              setOpenContract(false);
+            }}
+            keepMounted
+            fullWidth
+          >
+            <DialogTitle id="alert-dialog-title">
+              {`${sendProfile.name} さんからの依頼を受諾します`}
+            </DialogTitle>
+            <DialogContent>
+              <DialogContentText id="alert-dialog-description">
+                料金を決めてください
+              </DialogContentText>
+              <TextField
+                autoFocus
+                margin="dense"
+                id="name"
+                label="price"
+                placeholder="0~"
+                type="number"
+                fullWidth
+                variant="standard"
+                autoComplete="off"
+                onChange={(e) => {
+                  setPrice(Number(e.target.value));
+                }}
+              />
+            </DialogContent>
+            <DialogActions>
+              <Button
+                onClick={() => {
+                  setOpenContract(false);
+                }}
+              >
+                チャットに戻る
+              </Button>
+              <Button
+                onClick={() => {
+                  setOpenContract(false);
+                  contractPost();
+                }}
+                autoFocus
+                color="error"
+              >
+                確定
+              </Button>
+            </DialogActions>
+          </Dialog>
+        </>
+      ) : (
+        <>
+          <Button
+            color="error"
+            onClick={() => {
+              setOpenNone(true);
+            }}
+          >
+            マッチ解除
+          </Button>
+          <Dialog
+            open={openNone}
+            onClose={() => {
+              setOpenNone(false);
+            }}
+            keepMounted
+            fullWidth
+          >
+            <DialogTitle id="alert-dialog-title">
+              {"マッチを解除します"}
+            </DialogTitle>
+            <DialogContent>
+              <DialogContentText id="alert-dialog-description">
+                このユーザーとチャットを行うことができなくなります。
+                <br />
+                受諾せずHomeに戻りますか？
+              </DialogContentText>
+            </DialogContent>
+            <DialogActions>
+              <Button
+                onClick={() => {
+                  setOpenNone(false);
+                }}
+              >
+                チャットに戻る
+              </Button>
+              <Button
+                onClick={() => {
+                  setOpenNone(false);
+                  nonePost();
+                }}
+                autoFocus
+                color="error"
+              >
+                はい
+              </Button>
+            </DialogActions>
+          </Dialog>
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={() => {
+              setOpenContract(true);
+            }}
+          >
+            MENTA受諾
+          </Button>
+          <Dialog
+            open={openContract}
+            onClose={() => {
+              setOpenContract(false);
+            }}
+            keepMounted
+            fullWidth
+          >
+            <DialogTitle id="alert-dialog-title">
+              {`${sendProfile.name} さんからの依頼を受諾します`}
+            </DialogTitle>
+            <DialogContent>
+              <DialogContentText id="alert-dialog-description">
+                料金を決めてください
+              </DialogContentText>
+              <TextField
+                autoFocus
+                margin="dense"
+                id="name"
+                label="price"
+                placeholder="0~"
+                type="number"
+                fullWidth
+                variant="standard"
+                autoComplete="off"
+                onChange={(e) => {
+                  setPrice(Number(e.target.value));
+                }}
+              />
+            </DialogContent>
+            <DialogActions>
+              <Button
+                onClick={() => {
+                  setOpenContract(false);
+                }}
+              >
+                チャットに戻る
+              </Button>
+              <Button
+                onClick={() => {
+                  setOpenContract(false);
+                  contractPost();
+                }}
+                autoFocus
+                color="error"
+              >
+                確定
+              </Button>
+            </DialogActions>
+          </Dialog>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default MentaButtons;
