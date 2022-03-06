8ea8612ea0a8d71e14b045985ac93bd23a5ab0d9
mitsuYashi <hm20011027@gmail.com>
2022-03-07 03:45:48 +0900

    add:clientがlikeをしているか

--- api/app/controllers/likes_controller.rb
+++ api/app/controllers/likes_controller.rb
@@ -1,12 +1,18 @@
 class LikesController < ApplicationController
     def index
-        likes = Like.where(menta_id: params[:menta_id], status: true)
-        render json: likes
+        if likes = Like.find_by(client_id: params[:menta_id], status: true)
+            render json: "exist"
+        else likes = Like.where(menta_id: params[:menta_id], status: true)
+            render json: likes
+        end
     end
 
     def create
         if none = None.find_by(client_id: create_like_param[:client_id], menta_id: create_like_param[:menta_id])
             none.delete()
+        end
+        if like = Like.find_by(client_id: create_like_param[:client_id], status: true)
+            
         end
         if like = Like.find_by(client_id: create_like_param[:client_id], menta_id: create_like_param[:menta_id])
             like.update(status: true)
